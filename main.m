f25467cfcfb0397307124086638c3725ffa16a89
hanwpi <hany@usenhanym1.corp.emc.com>
2013-08-23 15:59:59 -0400

    Add new source file for testing

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PushClient
+//
+//  Created by Ying Han on 08/14/2013.
+//  Copyright 2013 EMC Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h> 
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
